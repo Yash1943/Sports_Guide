2c00301d08bb9884f5e51d110169463298b6f46f
Yash1943 <joshiyash1942003@gmail.com>
2024-08-04 10:52:42 +0530

    Team_Prefferances_add

--- Sports_Guide/settings.py
+++ Sports_Guide/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from . info import *
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,6 +90,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 LOGIN_URL = '/signin/'
 # Password validation
--- Sports_Guide/urls.py
+++ Sports_Guide/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from django.contrib.staticfiles.urls import staticfiles_pattern
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls'))
 ]
+# urlpatterns += staticfiles_pattern()
